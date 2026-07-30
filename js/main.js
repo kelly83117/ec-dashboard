b601c76d9d921d17210b57abcd231e352c250a57
jessica2000419 <jessica@playbox.com.tw>
2026-07-30 17:55:46 +0800

    fix: bump version to 305 (index.html/main.js were stuck at 304)

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 ﻿/* ES Module ?脣暺???靘?頛 app ??pages/* ??profit ??firebase */
 /* ?v= ???頝?index.html 鋆∠? app-version ??main.js?v= 銝韏瑟嚗???GitHub Pages ??   10 ?? JS 敹怠???雿輻???唳??????? */
-import './app.js?v=2026-07-30-304';
-import './pages/modal.js?v=2026-07-30-304';
-import './pages/dashboard.js?v=2026-07-30-304';
-import './pages/marketing.js?v=2026-07-30-304';
-import './pages/employees.js?v=2026-07-30-304';
-import './pages/daily.js?v=2026-07-30-304';
-import './pages/offices.js?v=2026-07-30-304';
-import './pages/users.js?v=2026-07-30-304';
-import './profit.js?v=2026-07-30-304';
-import './firebase.js?v=2026-07-30-304';
+import './app.js?v=2026-07-30-305';
+import './pages/modal.js?v=2026-07-30-305';
+import './pages/dashboard.js?v=2026-07-30-305';
+import './pages/marketing.js?v=2026-07-30-305';
+import './pages/employees.js?v=2026-07-30-305';
+import './pages/daily.js?v=2026-07-30-305';
+import './pages/offices.js?v=2026-07-30-305';
+import './pages/users.js?v=2026-07-30-305';
+import './profit.js?v=2026-07-30-305';
+import './firebase.js?v=2026-07-30-305';
+
 
 
 
