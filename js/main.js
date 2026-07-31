7c22a802fced6cab48bec1b4d33f84e6f3c9745b
jessica2000419 <jessica@playbox.com.tw>
2026-07-31 13:35:53 +0800

    v336: 確實移除採購工作總覽

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 ﻿/* ES Module ?脣暺???靘?頛 app ??pages/* ??profit ??firebase */
 /* ?v= ???頝?index.html 鋆∠? app-version ??main.js?v= 銝韏瑟嚗???GitHub Pages ??   10 ?? JS 敹怠???雿輻???唳??????? */
-import './app.js?v=2026-07-31-335';
-import './pages/modal.js?v=2026-07-31-335';
-import './pages/dashboard.js?v=2026-07-31-335';
-import './pages/marketing.js?v=2026-07-31-335';
-import './pages/employees.js?v=2026-07-31-335';
-import './pages/daily.js?v=2026-07-31-335';
-import './pages/offices.js?v=2026-07-31-335';
-import './pages/users.js?v=2026-07-31-335';
-import './profit.js?v=2026-07-31-335';
-import './firebase.js?v=2026-07-31-335';
+import './app.js?v=2026-07-31-336';
+import './pages/modal.js?v=2026-07-31-336';
+import './pages/dashboard.js?v=2026-07-31-336';
+import './pages/marketing.js?v=2026-07-31-336';
+import './pages/employees.js?v=2026-07-31-336';
+import './pages/daily.js?v=2026-07-31-336';
+import './pages/offices.js?v=2026-07-31-336';
+import './pages/users.js?v=2026-07-31-336';
+import './profit.js?v=2026-07-31-336';
+import './firebase.js?v=2026-07-31-336';
 
 
 
